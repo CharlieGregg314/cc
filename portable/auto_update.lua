1ca4e63a7ef70bc9cdbe4484b26dd1cc8b7835d2
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2023-06-11 20:00:33 +1000

    auto updater

--- portable/auto_update.lua
+++ portable/auto_update.lua
@@ -1,8 +1,38 @@
 
+function get_git(path)
+  return http.get("https://raw.githubusercontent.com/CharlieGregg314/cc/master/portable/"..path).readAll()
+end
 
+function split(value, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(value, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
 
-while true do
-  local event, text = os.pullEvent("paste")
-  print('"' .. text .. '" was pasted')
-  os.execute("pastebin")
+function update()
+  local files = split(get_git("file_list"), "\n")
+  for path in files do
+    local dir = string.match(path, ".+(?=/)")
+    -- local file = string.match(path, "[^/]+$")
+    if not fs.exists(dir) then
+      fs.makeDir(dir)
+    end
+    f = fs.open(path, "w")
+    f.write(get_git(path))
+    f.close()
+  end
+  return #files
+end
+
+local at_version = fs.open("version").readAll()
+local new_version = get_git("version")
+if at_version ~= new_version then
+  print("updating from version: "..at_version)
+  local files = update()
+  print("updated to version: "..new_version..", "..tostring(files).." files downloaded.")
 end
